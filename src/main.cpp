0bb3414254ced83cc7c4b3763610c1fa9a827631
Diego0275 <143463480+Diego0275@users.noreply.github.com>
2023-08-29 02:15:03 +0000

    Codigo inicial c++

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    for (int contador = 0; contador < 10; contador++)
+    {
+        std::cout<<"HOla"<<std::endl;
+    }
+    return 0;
+}
